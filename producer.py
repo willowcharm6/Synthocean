103115acc46306f6bf74433165408aa75a99f82f
crannash <tsd1309@gmail.com>
2024-10-05 12:47:39 -0500

    Update producer.py

--- producer.py
+++ producer.py
@@ -1,11 +1,8 @@
 import pygame
-import random
 
 class Producer(pygame.sprite.Sprite):
-    def __init__(self, screen_width, screen_height):
+    def __init__(self, x, y):
         super().__init__()
         self.image = pygame.Surface((20, 20))
         self.image.fill((0, 255, 0))  # Green color
-        self.rect = self.image.get_rect()
-        self.rect.x = random.randint(0, 780)
-        self.rect.y = random.randint(0, 580)
+        self.rect = self.image.get_rect(center=(x, y))  # Set the position of the producer
